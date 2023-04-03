976980cd705089beaff4386421530ec72d7456ce
Roberto Gambuzzi <gambuzzi@pop-os.localdomain>
2023-04-03 19:20:36 +0100

    first commit

---
+++ 2022ex19_plain.py
@@ -0,0 +1,227 @@
+_input = """Blueprint 1: Each ore robot costs 3 ore. Each clay robot costs 3 ore. Each obsidian robot costs 2 ore and 15 clay. Each geode robot costs 3 ore and 9 obsidian.
+Blueprint 2: Each ore robot costs 4 ore. Each clay robot costs 4 ore. Each obsidian robot costs 4 ore and 12 clay. Each geode robot costs 4 ore and 19 obsidian.
+Blueprint 3: Each ore robot costs 4 ore. Each clay robot costs 4 ore. Each obsidian robot costs 4 ore and 14 clay. Each geode robot costs 3 ore and 16 obsidian.
+Blueprint 4: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 2 ore and 17 clay. Each geode robot costs 3 ore and 19 obsidian.
+Blueprint 5: Each ore robot costs 4 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 10 clay. Each geode robot costs 2 ore and 10 obsidian.
+Blueprint 6: Each ore robot costs 3 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 16 clay. Each geode robot costs 3 ore and 9 obsidian.
+Blueprint 7: Each ore robot costs 4 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 7 clay. Each geode robot costs 3 ore and 9 obsidian.
+Blueprint 8: Each ore robot costs 3 ore. Each clay robot costs 3 ore. Each obsidian robot costs 2 ore and 19 clay. Each geode robot costs 2 ore and 12 obsidian.
+Blueprint 9: Each ore robot costs 3 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 16 clay. Each geode robot costs 3 ore and 20 obsidian.
+Blueprint 10: Each ore robot costs 2 ore. Each clay robot costs 2 ore. Each obsidian robot costs 2 ore and 7 clay. Each geode robot costs 2 ore and 14 obsidian.
+Blueprint 11: Each ore robot costs 2 ore. Each clay robot costs 4 ore. Each obsidian robot costs 2 ore and 16 clay. Each geode robot costs 4 ore and 12 obsidian.
+Blueprint 12: Each ore robot costs 2 ore. Each clay robot costs 4 ore. Each obsidian robot costs 4 ore and 15 clay. Each geode robot costs 2 ore and 15 obsidian.
+Blueprint 13: Each ore robot costs 4 ore. Each clay robot costs 4 ore. Each obsidian robot costs 4 ore and 5 clay. Each geode robot costs 2 ore and 10 obsidian.
+Blueprint 14: Each ore robot costs 4 ore. Each clay robot costs 4 ore. Each obsidian robot costs 2 ore and 15 clay. Each geode robot costs 3 ore and 16 obsidian.
+Blueprint 15: Each ore robot costs 3 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 19 clay. Each geode robot costs 2 ore and 9 obsidian.
+Blueprint 16: Each ore robot costs 4 ore. Each clay robot costs 3 ore. Each obsidian robot costs 4 ore and 15 clay. Each geode robot costs 4 ore and 9 obsidian.
+Blueprint 17: Each ore robot costs 4 ore. Each clay robot costs 3 ore. Each obsidian robot costs 2 ore and 17 clay. Each geode robot costs 3 ore and 16 obsidian.
+Blueprint 18: Each ore robot costs 3 ore. Each clay robot costs 3 ore. Each obsidian robot costs 2 ore and 13 clay. Each geode robot costs 3 ore and 12 obsidian.
+Blueprint 19: Each ore robot costs 4 ore. Each clay robot costs 3 ore. Each obsidian robot costs 2 ore and 10 clay. Each geode robot costs 4 ore and 10 obsidian.
+Blueprint 20: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 17 clay. Each geode robot costs 3 ore and 10 obsidian.
+Blueprint 21: Each ore robot costs 4 ore. Each clay robot costs 3 ore. Each obsidian robot costs 2 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
+Blueprint 22: Each ore robot costs 3 ore. Each clay robot costs 4 ore. Each obsidian robot costs 3 ore and 20 clay. Each geode robot costs 3 ore and 14 obsidian.
+Blueprint 23: Each ore robot costs 2 ore. Each clay robot costs 2 ore. Each obsidian robot costs 2 ore and 17 clay. Each geode robot costs 2 ore and 10 obsidian.
+Blueprint 24: Each ore robot costs 3 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 19 clay. Each geode robot costs 3 ore and 19 obsidian.
+Blueprint 25: Each ore robot costs 3 ore. Each clay robot costs 4 ore. Each obsidian robot costs 2 ore and 14 clay. Each geode robot costs 3 ore and 14 obsidian.
+Blueprint 26: Each ore robot costs 3 ore. Each clay robot costs 4 ore. Each obsidian robot costs 3 ore and 13 clay. Each geode robot costs 3 ore and 19 obsidian.
+Blueprint 27: Each ore robot costs 4 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 7 clay. Each geode robot costs 2 ore and 7 obsidian.
+Blueprint 28: Each ore robot costs 4 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 20 clay. Each geode robot costs 2 ore and 19 obsidian.
+Blueprint 29: Each ore robot costs 4 ore. Each clay robot costs 4 ore. Each obsidian robot costs 4 ore and 16 clay. Each geode robot costs 2 ore and 15 obsidian.
+Blueprint 30: Each ore robot costs 2 ore. Each clay robot costs 4 ore. Each obsidian robot costs 4 ore and 19 clay. Each geode robot costs 2 ore and 18 obsidian."""
+
+from collections import deque
+import re
+
+
+def parser(s):
+    ret = []
+    for line in s.splitlines():
+        parsed_line = re.findall(
+            r"Blueprint \d+: Each (\w+) robot costs (\d+) (\w+). Each (\w+) robot costs (\d+) (\w+). Each (\w+) robot costs (\d+) (\w+) and (\d+) (\w+). Each (\w+) robot costs (\d+) (\w+) and (\d+) (\w+).",
+            line,
+            re.I | re.S,
+        )[0]
+        # print(parsed_line)
+        d = {}
+        inside = {}
+        inside["ore"] = int(parsed_line[1])
+        d["ore"] = inside
+        inside = {}
+        inside["ore"] = int(parsed_line[4])
+        d["clay"] = inside
+        inside = {}
+        inside["ore"] = int(parsed_line[7])
+        inside["clay"] = int(parsed_line[9])
+        d["obsidian"] = inside
+        inside = {}
+        inside["ore"] = int(parsed_line[12])
+        inside["obsidian"] = int(parsed_line[14])
+        d["geode"] = inside
+
+        ret.append(d)
+        # ret.append(
+        #     {
+        #         'ore': {'ore': int(parsed_line[1])},
+        #         'clay': {'ore': int(parsed_line[4])},
+        #         'obsidian': {'ore': int(parsed_line[7]),
+        #                      'clay': int(parsed_line[9])},
+        #         'geode': {'ore': int(parsed_line[12]),
+        #                      'obsidian': int(parsed_line[14])},
+        #     }
+        # )
+    return ret
+
+
+def run_blueprint(bp, time_limit=24):
+    max_ore = max([x["ore"] for x in bp.values()])
+    max_clay = max([x.get("clay", 0) for x in bp.values()])
+    max_obsidian = max([x.get("obsidian", 0) for x in bp.values()])
+    bp_ore_ore = bp["ore"]["ore"]
+    bp_clay_ore = bp["clay"]["ore"]
+    bp_o_o = bp["obsidian"]["ore"]
+    bp_o_c = bp["obsidian"]["clay"]
+    bp_gore = bp["geode"]["ore"]
+    bp_go = bp["geode"]["obsidian"]
+
+    # robots, resources, timer
+    stack = deque([(1, 0, 0, 0, 0, 0, 0, 0, 0)])
+    seen = set()
+    ret_max = 0
+    gen = 0
+    geodes_at_gen = 0
+
+    while stack:
+        # print(len(stack), end=" ")
+        state = (
+            robots_ore,
+            robots_clay,
+            robots_obsidian,
+            robots_geode,
+            resources_ore,
+            resources_clay,
+            resources_obsidian,
+            resources_geode,
+            timer,
+        ) = stack.popleft()
+
+        if state in seen:
+            continue
+        seen.add(state)
+
+        if timer == time_limit:
+            if resources_geode > ret_max:
+                ret_max = resources_geode
+                # print(f"geode {resources_geode}")
+        else:
+            gen = max(gen, timer)
+            geodes_at_gen = max(robots_geode, geodes_at_gen)
+
+            if timer == gen and robots_geode < geodes_at_gen:
+                continue
+
+            # no robot build, just storage
+            stack.append(
+                (
+                    robots_ore,
+                    robots_clay,
+                    robots_obsidian,
+                    robots_geode,
+                    robots_ore + resources_ore,
+                    robots_clay + resources_clay,
+                    robots_obsidian + resources_obsidian,
+                    robots_geode + resources_geode,
+                    timer + 1,
+                )
+            )
+            # robot build
+            if bp_ore_ore <= resources_ore and robots_ore < max_ore:
+                stack.append(
+                    (
+                        robots_ore + 1,
+                        robots_clay,
+                        robots_obsidian,
+                        robots_geode,
+                        robots_ore + resources_ore - bp_ore_ore,
+                        robots_clay + resources_clay,
+                        robots_obsidian + resources_obsidian,
+                        robots_geode + resources_geode,
+                        timer + 1,
+                    )
+                )
+            if bp_clay_ore <= resources_ore and robots_clay < max_clay:
+                stack.append(
+                    (
+                        robots_ore,
+                        robots_clay + 1,
+                        robots_obsidian,
+                        robots_geode,
+                        robots_ore + resources_ore - bp_clay_ore,
+                        robots_clay + resources_clay,
+                        robots_obsidian + resources_obsidian,
+                        robots_geode + resources_geode,
+                        timer + 1,
+                    )
+                )
+            if (
+                bp_o_o <= resources_ore
+                and bp_o_c <= resources_clay
+                and robots_obsidian < max_obsidian
+            ):
+                stack.append(
+                    (
+                        robots_ore,
+                        robots_clay,
+                        robots_obsidian + 1,
+                        robots_geode,
+                        robots_ore + resources_ore - bp_o_o,
+                        robots_clay + resources_clay - bp_o_c,
+                        robots_obsidian + resources_obsidian,
+                        robots_geode + resources_geode,
+                        timer + 1,
+                    )
+                )
+            if bp_gore <= resources_ore and bp_go <= resources_obsidian:
+                stack.append(
+                    (
+                        robots_ore,
+                        robots_clay,
+                        robots_obsidian,
+                        robots_geode + 1,
+                        robots_ore + resources_ore - bp_gore,
+                        robots_clay + resources_clay,
+                        robots_obsidian + resources_obsidian - bp_go,
+                        robots_geode + resources_geode,
+                        timer + 1,
+                    )
+                )
+    return ret_max
+
+
+print("plain")
+
+from multiprocessing import Pool
+import time
+from pyo3_streaming_exercise import run_blueprint as rust_run
+
+t0 = time.monotonic()
+ret = 0
+
+def parallel_function(i_blueprint):
+    i, blueprint = i_blueprint
+    n = rust_run(blueprint, 24)
+    print(i, n)
+    return (i + 1) * n
+
+with Pool(8) as p:
+    ret = sum(
+        p.map(parallel_function, enumerate(parser(_input)))
+    )
+
+# for i, blueprint in enumerate(parser(_input)):
+#     n = rust_run(blueprint, 24)
+#     print(i, n)
+#     ret += (i + 1) * n
+
+print(time.monotonic() - t0, ret)
+
+input()
---
+++ src/lib.rs
@@ -0,0 +1,217 @@
+use std::collections::{HashSet, VecDeque};
+
+use pyo3::prelude::*;
+use pyo3::types::PyDict;
+
+#[derive(Debug, Hash, PartialEq, Eq, Clone, Default)]
+struct State {
+    robots_ore: u32,
+    robots_clay: u32,
+    robots_obsidian: u32,
+    robots_geode: u32,
+    resources_ore: u32,
+    resources_clay: u32,
+    resources_obsidian: u32,
+    resources_geode: u32,
+    timer: u32,
+}
+
+impl State {
+    fn new(
+        robots_ore: u32,
+        robots_clay: u32,
+        robots_obsidian: u32,
+        robots_geode: u32,
+        resources_ore: u32,
+        resources_clay: u32,
+        resources_obsidian: u32,
+        resources_geode: u32,
+        timer: u32,
+    ) -> Self {
+        State {
+            robots_ore,
+            robots_clay,
+            robots_obsidian,
+            robots_geode,
+            resources_ore,
+            resources_clay,
+            resources_obsidian,
+            resources_geode,
+            timer,
+        }
+    }
+}
+
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn run_blueprint(blueprint: &PyDict, time_limit: u32) -> PyResult<u32> {
+    let bp_ore_ore: u32 = blueprint
+        .get_item("ore")
+        .unwrap()
+        .get_item("ore")
+        .unwrap()
+        .extract()
+        .unwrap();
+    let bp_clay_ore: u32 = blueprint
+        .get_item("clay")
+        .unwrap()
+        .get_item("ore")
+        .unwrap()
+        .extract()
+        .unwrap();
+    let bp_o_o: u32 = blueprint
+        .get_item("obsidian")
+        .unwrap()
+        .get_item("ore")
+        .unwrap()
+        .extract()
+        .unwrap();
+    let bp_o_c: u32 = blueprint
+        .get_item("obsidian")
+        .unwrap()
+        .get_item("clay")
+        .unwrap()
+        .extract()
+        .unwrap();
+    let bp_gore: u32 = blueprint
+        .get_item("geode")
+        .unwrap()
+        .get_item("ore")
+        .unwrap()
+        .extract()
+        .unwrap();
+    let bp_go: u32 = blueprint
+        .get_item("geode")
+        .unwrap()
+        .get_item("obsidian")
+        .unwrap()
+        .extract()
+        .unwrap();
+
+    let max_ore = bp_ore_ore.max(bp_clay_ore).max(bp_o_o).max(bp_gore);
+    let max_clay = bp_o_c;
+    let max_obsidian = bp_go;
+
+    let mut stack: VecDeque<State> = VecDeque::new();
+    stack.push_back(State {
+        robots_ore: 1,
+        robots_clay: 0,
+        robots_obsidian: 0,
+        robots_geode: 0,
+        resources_ore: 0,
+        resources_clay: 0,
+        resources_obsidian: 0,
+        resources_geode: 0,
+        timer: 0,
+    });
+    let mut seen: HashSet<State> = Default::default();
+
+    let mut ret_max = 0;
+    let mut gen = 0;
+    let mut geodes_at_gen = 0;
+
+    while let Some(state) = stack.pop_front() {        
+        if seen.contains(&state) {
+            continue;
+        }
+        seen.insert(state.clone());
+
+        if state.timer == time_limit {
+            if state.resources_geode > ret_max {
+                ret_max = state.resources_geode;
+            }
+            continue;
+        }
+
+        gen = gen.max(state.timer);
+        geodes_at_gen = geodes_at_gen.max(state.robots_geode);
+
+        if state.timer == gen && state.robots_geode < geodes_at_gen {
+            continue;
+        }
+
+        // no robot build, just storage
+        stack.push_back(State::new(
+            state.robots_ore,
+            state.robots_clay,
+            state.robots_obsidian,
+            state.robots_geode,
+            state.robots_ore + state.resources_ore,
+            state.robots_clay + state.resources_clay,
+            state.robots_obsidian + state.resources_obsidian,
+            state.robots_geode + state.resources_geode,
+            state.timer + 1,
+        ));
+
+        // create an ore robot
+        if bp_ore_ore <= state.resources_ore && state.robots_ore < max_ore {
+            stack.push_back(State::new(
+                state.robots_ore+1,
+                state.robots_clay,
+                state.robots_obsidian,
+                state.robots_geode,
+                state.robots_ore + state.resources_ore- bp_ore_ore,
+                state.robots_clay + state.resources_clay,
+                state.robots_obsidian + state.resources_obsidian,
+                state.robots_geode + state.resources_geode,
+                state.timer + 1,
+            ));
+        }
+
+        // clay robot
+        if bp_clay_ore <= state.resources_ore && state.robots_clay < max_clay {
+            stack.push_back(State::new(
+                state.robots_ore,
+                state.robots_clay+1,
+                state.robots_obsidian,
+                state.robots_geode,
+                state.robots_ore + state.resources_ore- bp_clay_ore,
+                state.robots_clay + state.resources_clay,
+                state.robots_obsidian + state.resources_obsidian,
+                state.robots_geode + state.resources_geode,
+                state.timer + 1,
+            ));
+        }
+
+        //obsidian robot
+        if bp_o_o <= state.resources_ore
+            && bp_o_c <= state.resources_clay
+            && state.robots_obsidian < max_obsidian {
+            stack.push_back(State::new(
+                state.robots_ore,
+                state.robots_clay,
+                state.robots_obsidian+1,
+                state.robots_geode,
+                state.robots_ore + state.resources_ore- bp_o_o,
+                state.robots_clay + state.resources_clay- bp_o_c,
+                state.robots_obsidian + state.resources_obsidian,
+                state.robots_geode + state.resources_geode,
+                state.timer + 1,
+            ));
+        }
+
+        //geode robot
+        if bp_gore <= state.resources_ore && bp_go <= state.resources_obsidian {
+            stack.push_back(State::new(
+                state.robots_ore,
+                state.robots_clay,
+                state.robots_obsidian,
+                state.robots_geode+1,
+                state.robots_ore + state.resources_ore-bp_gore,
+                state.robots_clay + state.resources_clay,
+                state.robots_obsidian + state.resources_obsidian- bp_go,
+                state.robots_geode + state.resources_geode,
+                state.timer + 1,
+            ));
+        }
+    }
+
+    Ok(ret_max)
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn pyo3_streaming_exercise(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(run_blueprint, m)?)?;
+    Ok(())
+}
